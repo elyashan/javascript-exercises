f3c56dbdba7499db5b5bb49c6a12b289afe66574
elyashan <fakirullah.seckin@boun.edu.tr>
2025-12-20 11:58:18 +0300

    solved 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, cur) => {
+    acc += cur;
+    return acc;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, cur) => {
+    acc *= cur;
+    return acc;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
