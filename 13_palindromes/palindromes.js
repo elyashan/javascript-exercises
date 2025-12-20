85ce15bc69b91c545191a3ac834405aaa7e9edb2
elyashan <fakirullah.seckin@boun.edu.tr>
2025-12-20 12:17:37 +0300

    solved 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const allowed = "abcdefghijklmnoprstuvwxyz1234567890";
+  const a = "asd";
+  const newStr = str
+    .toLowerCase()
+    .split("")
 
+    .filter((el) => allowed.includes(el))
+    .join("");
+
+  return newStr.split("").reverse().join("") == newStr;
 };
 
 // Do not edit below this line
