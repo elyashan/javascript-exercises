22e720e6398b4b69776b9acec173f47a39cecadb
elyashan <fakirullah.seckin@boun.edu.tr>
2025-12-20 12:39:11 +0300

    solved 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let fib = 1;
+  let firstPrev = 1;
+  let secondPrev = 0;
+  if (num == 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  let a = "asd";
 
+  if (typeof num == "String") {
+    Number(num);
+  }
+  for (let i = 2; i <= num; i++) {
+    let cur = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = cur;
+
+    // [1,1,2,3,5,8,13]
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
